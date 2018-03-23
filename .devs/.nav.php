ff17af87ebcb687dec6c341d7ec1332f280e1f12
GoldFash <goldfashhosting@users.noreply.github.com>
2018-03-23 03:11:37 -0400

    Create .nav.php

---
+++ .devs/.nav.php
@@ -0,0 +1,67 @@
+	<div class="preloader"><span class="preloader-gif"></span></div>
+	<div class="theme-wrap clearfix">
+		<!--================================responsive log and menu==========================================-->
+		<div class="wsmenucontent overlapblackbg"></div>
+		<div class="wsmenuexpandermain slideRight">
+			<a id="navToggle" class="animated-arrow slideLeft"><span></span></a>
+			<a href="#" class="smallogo"><img src="images/suJse_L.png" width="120" alt="" /></a>
+		</div>
+		<!--================================HEADER==========================================-->
+		<div class="header solid">
+			<div class="header-inner">
+				
+				<div class="nav-wrapper"><!--main navigation-->
+					<div class="container">
+						<div class="top-toolbar"><!--header toolbar-->
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12 col-sm-12 col-xs-12 pull-right">
+								<div class="top-contact-info">
+									<ul>
+										<li class="toolbar-email"><i class="fa fa-envelope-o"></i> <? echo $JesusSiteSettings{contactEmail}; ?></li>
+										<li class="toolbar-contact"><i class="fa fa-phone"></i><? echo $JesusSiteSettings{contactNumber}; ?></li>
+										
+									</ul>
+								</div>
+								<div class="social-content">
+									<ul class="social-links">
+										
+										<li><a class="twitter" href="<? echo $JesusSiteSettings{twitter}; ?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li><a class="facebook" href="<? echo $JesusSiteSettings{facebook}; ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										
+										<li><button class = "login" data-toggle = "modal" data-target = "#login">login</button></li>
+										<li><button class = "register" data-toggle = "modal" data-target = "#register">register</button></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div><!--header toolbar end-->
+						<!--Main Menu HTML Code-->
+						<nav class="wsmenu slideLeft clearfix">
+							<div class="logo pull-left"><a href="index.html" title="Responsive Slide Menus"><img src="images/suJse_L.png" width="175" alt="" /></a></div>
+							<ul class="mobile-sub wsmenu-list pull-right">
+								<li><a href="?0_0=hola" class="">Home</a>
+									
+								</li>
+								<li><a href="?0_0=pricing" class="">Pricing</a></li>
+								<li><a href="category.html">categories</a></li>
+								<li><a href="listing-archive.html">listings</a></li>
+								<li><a href="#">pages</a>
+									<ul class="wsmenu-submenu">
+										<li><a href="about.html">About</a></li>
+										<li><a href="blog.html">Blog</a></li>
+										<li><a href="single.html">Blog Single</a></li>
+										<li><a href="single-listing.html">listing Single</a></li>
+										<li><a href="404.html">Error 404</a></li>
+										
+									</ul>
+								</li>
+							  <li><a href="contact.html">Contact Us</a></li>
+							  <li><a class="toolbar-new-listing" href="#"> Add Listing</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div><!--main navigation end-->
+			</div>
+		</div><!-- header end -->
