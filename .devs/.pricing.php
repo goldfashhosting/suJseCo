7f5f7fa3a0991059de55f264b2a70c0acd0a6588
GoldFash <goldfashhosting@users.noreply.github.com>
2018-03-23 03:11:07 -0400

    Added Pricing WireFrame

---
+++ .devs/.pricing.php
@@ -0,0 +1,94 @@
+<!--================================PAGE TITLE==========================================-->
+			<div class="page-title-wrap padding-top-30 padding-bottom-30 margin-bottom-100"><!-- section title -->
+				<div class="container">
+					<div class="pagetitle-text">
+						<h4 class="white">OUR PRICING</h4>
+					</div>
+					<div class="pagetitle-breadcrumb">
+						<p class="white"><a href="?0_0=hola">Home</a> / <a href="?0_0=pricing">Business Pricing</a></p>
+					</div>
+				</div>
+			</div><!-- section title end -->
+			<!--================================PRICING PLAN SECTION 2==========================================-->
+		
+			<section class="pricing-section padding-bottom-40">
+				<div class="container"><!-- section container -->
+					<div class="row pricing-wrap style-1 clearfix">
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4"><!-- .pricing -->
+							<div class="pricing shadow-1 clearfix">
+								<div class="pricing-inner">
+									<div class="price-plan">
+										<p class="currency c444"></p>
+										<p class="price c444">FREE</p>
+										<p class="month c444"></p>
+									</div>
+									<div class="pricing-content">
+										<ul>
+											<li>1 Premium suJse</li>
+											<li>1 Featured Listing</li>
+											<li>1 Sticky Listing</li>
+											<li>14 Regular Listings</li>
+										</ul>
+										<div class="btn-1 margin-top-30">
+											<a class="white bgblue-1" href="#">Choose Plan</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div><!-- pricing end -->
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4"><!-- .pricing -->
+							<div class="pricing shadow-1 clearfix">
+								<div class="pricing-inner">
+									<div class="price-plan">
+										<p class="currency c444">$</p>
+										<p class="price c444">7</p>
+										<p class="month c444">/wk</p>
+									</div>
+									<div class="pricing-content">
+										<ul>
+											<li>2 Premium suJse&#39;s</li>
+											<li>3 Featured Listings</li>
+											<li>1 Sticky Listing</li>
+											<li> suJse&#39;s B2B Lending</li>
+											<li>1 Professional Landing Page</li>
+											<li>100 FREE Gold2GK SMS Credits</li>
+											<li>Unlimited Regular Listings</li>
+										</ul>
+										<div class="btn-1 margin-top-30">
+											<a class="white bgblue-1" href="#">Choose Plan</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div><!-- pricing end -->
+						<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4"><!-- .pricing -->
+							<div class="pricing shadow-1 clearfix">
+								<div class="pricing-inner">
+									<div class="price-plan">
+										<p class="currency c444">$</p>
+										<p class="price c444">37</p>
+										<p class="month c444">/mon</p>
+									</div>
+									<div class="pricing-content">
+										<ul>
+											<li>5 Premium suJse&#39;s</li>
+											<li>7 Featured Listings</li>
+											<li>4 Sticky Listings</li>
+											<li> suJse&#39;s B2B Lending</li>
+											<li>1 Professional Landing Page</li>
+											<li>100 FREE Gold2GK SMS Credits</li>
+											<li>1 Email Blast</li>
+											<li>1 Social Media Blast</li>
+											<li>Unlimited Regular Listings</li>
+										</ul>
+										<div class="btn-1 margin-top-30">
+											<a class="white bgblue-1" href="#">Choose Plan</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div><!-- pricing end -->
+					</div><!-- .row pricing end -->
+				</div><!-- container end -->
+			</section>
+		</div><!--/sc-page-->
